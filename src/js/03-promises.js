47fe3a8c0e35e44dcf1f71e41278a8d8d227bfe6
SophiaTretiakova <122561236+SophiaTretiakova@users.noreply.github.com>
2023-07-09 23:05:20 +0300

    task-3 completed

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,49 @@
+import Notiflix from 'notiflix';
+const refs = {
+  formEl: document.querySelector('.form'),
+};
+
+refs.formEl.addEventListener('submit', handlSub);
+
+function handlSub(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+  let delayValue = +delay.value;
+  let stepValue = +step.value;
+  let amountValue = +amount.value;
+  for (let i = 1; i <= amountValue; i++) {
+    createPromise(i, delayValue);
+    delayValue += stepValue;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`,
+            { useIcon: false }
+          )
+        );
+      }, delay);
+    });
   } else {
-    // Reject
+    return new Promise(reject => {
+      setTimeout(
+        () =>
+          reject(
+            Notiflix.Notify.failure(
+              `❌ Rejected promise ${position} in ${delay}ms`,
+              { useIcon: false }
+            )
+          ),
+        delay
+      );
+    });
   }
 }
